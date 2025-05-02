1711cea95b7c8f36156ccbfe10a7113865bf84d6
hzlucasf <hzlucasf@gmail.com>
2025-05-02 10:37:29 -0300

    feat: add constants/

---
+++ constants/src/main.rs
@@ -0,0 +1,9 @@
+const PROGRAMMING_LANGUAGE: &str = "Rust";
+
+const YEAR: u16 = 2025;
+
+fn main() {
+    println!("`PROGRAMMING_LANGUAGE`: {:?}", PROGRAMMING_LANGUAGE);
+
+    println!("`YEAR`: {}", YEAR);
+}
