dcefbea1f544a03b1863d5639d267e13f6ae80c7
hzlucasf <hzlucasf@gmail.com>
2025-05-03 16:47:40 -0300

    feat: add scalar-data-types/

---
+++ scalar-data-types/src/main.rs
@@ -0,0 +1,21 @@
+// u8 | i8
+// u16 | i16
+// u32 | i32 (default)
+// u64 | i64
+// u128 | i128
+
+// f32 | f64 (default)
+
+// char
+
+// bool
+
+fn main() {
+    let _i = 0;
+
+    let _f = 3.141592;
+
+    let _c = 'R';
+
+    let _b = true;
+}
