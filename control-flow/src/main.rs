083068fb2a6d30640fd73a907771d0f5441a6291
hzlucasf <hzlucasf@gmail.com>
2025-05-07 19:21:04 -0300

    feat: add control-flow/

---
+++ control-flow/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let programming_language = "rust";
+
+    if programming_language == "rust" {
+        println!("`programming_language`: {:?}", programming_language);
+    }
+
+    let b = true;
+
+    if b {
+        println!("`b`: true");
+    } else {
+        println!("`b`: false");
+    }
+}
